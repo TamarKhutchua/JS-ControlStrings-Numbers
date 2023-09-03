90aac60022f68cbbe841b5d5867efcecb9ed8ee3
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-03 12:34:24 +0400

    round decimals

--- app.js
+++ app.js
@@ -16,3 +16,12 @@ square *= -1;
 cube *= -1;
 console.log("\nLargest Negative: " + Math.max(square, cube));
 console.log("\nSmallest Negative: " + Math.min(square, cube));
+
+let bodyTemp = 98.6;
+console.log("Ceiling: " + Math.ceil(bodyTemp));
+console.log("Floor: " + Math.floor(bodyTemp));
+console.log("Round: " + Math.round(bodyTemp));
+console.log("\nImprecision: " + 81.66 * 15);
+console.log("Corrected: " + (81.66 * 100 * 15) / 100);
+console.log("\nFloat: " + Math.PI);
+console.log("Commuted: " + Math.round(Math.PI * 100) / 100);
