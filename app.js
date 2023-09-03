b59b708a8c37d25d662dba3a5fa5111ae80892a6
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-03 12:49:28 +0400

    generate randoms

--- app.js
+++ app.js
@@ -25,3 +25,25 @@ console.log("\nImprecision: " + 81.66 * 15);
 console.log("Corrected: " + (81.66 * 100 * 15) / 100);
 console.log("\nFloat: " + Math.PI);
 console.log("Commuted: " + Math.round(Math.PI * 100) / 100);
+
+let i,
+  rand,
+  temp,
+  nums = [];
+let str = "\n\nYour Six Lucky Numbers: ";
+for (i = 1; i < 60; i++) {
+  nums[i] = i;
+}
+for (i = 1; i < 60; i++) {
+  rand = Math.ceil(Math.random() * 59);
+  temp = nums[i];
+  nums[i] = nums[rand];
+  nums[rand] = temp;
+}
+for (i = 1; i < 7; i++) {
+  str += nums[i];
+  if (i !== 6) {
+    str += " - ";
+  }
+}
+console.log(str);
